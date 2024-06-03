a7ee29e869226b9c8a8d8fe7befa1cf3642fee0c
joseCarlosAndrade <josecarlos.101@usp.br>
2024-06-03 13:54:48 -0300

    param included

--- src/smb_highlevel_controller/src/scan_listener.cpp
+++ src/smb_highlevel_controller/src/scan_listener.cpp
@@ -1,6 +1,7 @@
 #include"ros/ros.h"
 #include"sensor_msgs/LaserScan.h"
 #include<vector>
+#include<stdlib.h>
 
 void laserscan_callback(const sensor_msgs::LaserScan& msg) {
     
@@ -17,9 +18,17 @@ void laserscan_callback(const sensor_msgs::LaserScan& msg) {
 int main(int argc, char ** argv) {
     ros::init(argc, argv, "scan_listener");
 
+    // pegando parametros
+    std::string topic_name;
+    int queue;
     ros::NodeHandle n("~");
+    
+    if (!n.getParam("/my_scan_listener/topic/name", topic_name) || !n.getParam("/my_scan_listener/topic/queue", queue)) {
+        ROS_ERROR("Parâmetros não encontrados!");
+        exit(EXIT_FAILURE);
+    }
 
-    ros::Subscriber laser_listener = n.subscribe("/scan", 10, laserscan_callback);
+    ros::Subscriber laser_listener = n.subscribe(topic_name, queue, laserscan_callback);
 
     ros::spin();
     return 0;
